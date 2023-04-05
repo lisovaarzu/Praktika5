7fb030152f00ab007abd3b279beb3208848ee2f2
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-04-05 18:46:44 +1000

    Add files via upload

---
+++ StrokiNaSlova/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StrokiNaSlova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' });
+
+            foreach (string str in words)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
