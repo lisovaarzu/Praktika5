0078fe0d80527edb539f79b477ce39b682a623bf
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-04-21 18:46:28 +1000

    Add files via upload

---
+++ PerestanovkaSlov/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PerestanovkaSlov
+{
+    internal class Program
+    {
+        static string ReverseWord(string[] text)
+        {
+            Array.Reverse(text);
+            string str = string.Join(" ", text);
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' });
+            string text2 = ReverseWord(words);
+
+            foreach (string str in words)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
