ecb2893b39094221194dbb90559736101d7aca71
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-05-11 17:53:44 +1000

    Add files via upload

---
+++ PerestanovkaSlov/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PerestanovkaSlov
+{
+    internal class Program
+    {
+        static string ReverseWord(string text)
+        {
+            var words1 = SplitText(text);
+            Array.Reverse(words1);
+            return string.Join(" ", words1);
+        }
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(new char[] { ' ' });
+            return words;
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] result = text.Split(new char[] { ' ' });
+            string words = ReverseWord(text);
+
+            foreach (string str in result)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine(words);
+            Console.ReadLine();
+        }
+    }
+}
